7d00fb67e63d71f3b9c642b7575c839425483b59
noesterle <noesterle1@gmail.com>
2025-01-14 23:37:04 -0500

    Added outline of education.

--- app/ui/ResumeEducation.tsx
+++ app/ui/ResumeEducation.tsx
@@ -3,7 +3,19 @@
 export default function ResumeEducation() {
     return(
         <div>
-            Education
+            <span>Education</span>
+            <hr/>
+            <div className="flex">
+                <div className="inline-block flex-auto">Rochester Institute of Technology</div>
+                <div className="inline-block">
+                    <span>Aug 2014 - May 2020</span></div>
+            </div>
+            <div>
+                <span>Bachelors of Science Software Engineering 2020</span>
+            </div>
+            <div>
+                <span>Advanced Courses: Senior Project; Process and Project Management; Human Centered Requirements and Design; Software Testing; Performance Engineering; Analysis of Algorithms</span>
+            </div>
         </div>
     );
 }
