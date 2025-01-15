129f76ab4b4089c045b9cc14e08834b70bf7ed92
noesterle <noesterle1@gmail.com>
2025-01-14 23:42:08 -0500

    Add outline for Resume Skills.

--- app/ui/ResumeSkills.tsx
+++ app/ui/ResumeSkills.tsx
@@ -3,7 +3,17 @@
 export default function ResumeSkills() {
     return(
         <div>
-            Skills
+            <span>Skills</span>
+            <hr/>
+            <div>
+                <span>LANGUAGES:</span><span>Python, Java, C, Rust, SQL, Bash, Angular, TypeScript, HTML,
+                Ruby, JUnit, Flask, Django, Dataweave, Docker Compose</span>
+                <br/>
+                <span>PLATFORMS:</span><span>Ubuntu, Windows, Red Hat Enterprise Linux, Android, Mulesoft,
+                Docker</span>
+                <br/>
+                <span>SOURCE CONTROL:</span><span>Git, Subversion</span>
+            </div>
         </div>
     );
 }
