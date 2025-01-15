594b73453c98a29e5d54f4995d95620eddf3e562
noesterle <noesterle1@gmail.com>
2025-01-14 23:37:39 -0500

    Add outline of Summary.

--- app/ui/ResumeSummary.tsx
+++ app/ui/ResumeSummary.tsx
@@ -3,7 +3,9 @@
 export default function ResumeSummary() {
     return(
         <div>
-            Summary
+            <span>Summary</span>
+            <hr/>
+            <span className="text-justify">To obtain a full-time position as a Software Engineer in order to solve challenging problems and deploy quality code that helps to improve people's digital experiences and lives.</span>
         </div>
     );
 }
